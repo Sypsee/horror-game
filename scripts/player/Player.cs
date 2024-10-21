2a7a22ed4328ec26c4c4c8dac42858834ba2d566
Sypsee <vitthalsoni6298@gmail.com>
2024-10-21 22:01:58 +0530

    Movement

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -1,9 +1,14 @@
+using System;
 using Godot;
 
 public partial class Player : CharacterBody3D
 {
 	private const float Speed = 5.0f;
-	private const float JumpVelocity = 4.5f;
+	private const float JumpVelocity = 5.5f;
+
+	private const float bob_freq = 2.0f;
+	private const float bob_ampl = 0.08f;
+	private double t_bob = 0.0f;
 
 	[Export] private Node3D head;
 	[Export] private Camera3D camera;
@@ -44,18 +49,40 @@ public partial class Player : CharacterBody3D
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		Vector3 direction = (head.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
+		if (IsOnFloor())
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			if (direction != Vector3.Zero)
+			{
+				velocity.X = direction.X * Speed;
+				velocity.Z = direction.Z * Speed;
+			}
+			else
+			{
+				velocity.X = Mathf.Lerp(velocity.X, direction.X * Speed, (float)delta * 8.0f);
+				velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * Speed, (float)delta * 8.0f);
+			}
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.Lerp(velocity.X, direction.X * Speed, (float)delta * 3.0f);
+			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * Speed, (float)delta * 3.0f);
 		}
+
+		t_bob += delta * velocity.Length() * (IsOnFloor() ? 1.0 : 0.0);
+		camera.Transform = HeadBob(t_bob);
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+    private Transform3D HeadBob(double time)
+	{
+		Transform3D transform = camera.Transform;
+		Vector3 origin = Vector3.Zero;
+		origin.Y = Mathf.Sin((float)time * bob_freq) * bob_ampl;
+		origin.X = Mathf.Cos((float)time * bob_freq / 2) * bob_ampl;
+
+		transform.Origin = origin;
+		return transform;
 	}
 }
